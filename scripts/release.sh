e22e3b568eaf5006319f518e8c279aecf891d7a1
boatbomber <zack@boatbomber.com>
2026-06-11 16:11:27 -0700

    Exclude jest config from wally package

--- scripts/release.sh
+++ scripts/release.sh
@@ -5,7 +5,5 @@ set -e
 rojo sourcemap sourcemap.project.json -o sourcemap.json
 
 darklua process --config default.darklua.json src/ dist/src
-rm -rf dist/src/__tests__
-rm -rf dist/src/jest.config.luau
 rojo build default.project.json -o CullThrottle.rbxm
 wally publish
